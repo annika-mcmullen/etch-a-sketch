2bbbf00b7e4c0f7afbcddbaf84f96775b0afbd4a
Annika McMullen <annikamcmullen@icloud.com>
2024-07-16 18:55:51 -0500

    Grid Adjustments: Added js logic to add another div to hold rows so grid stays at 16x16 even when browser size changes. Added CS styling to wrap the divs as needed and keep divs relatively square. Updated class names in index.html

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
 //JavaScript logic for etch-a-sketch
 
-let container = document.querySelector('#container');
+let container = document.querySelector('#containerDiv');
+
 
 //use a for loop to  create 16x16 divs and append to add to html
-for (let i=0; i < 16; i++){
-    for (let j=0; j<16; j++){
+// for (let i=0; i < 16; i++){
+//     for (let j=0; j<16; j++){
+//         let boxDiv = document.createElement('div');
+//         boxDiv.className= "grid-box";
+//         container.appendChild(boxDiv);
+//     }
+// }
+//creates a div for each row allowing graph to be limited to 16x16
+for (let i = 0; i < 16; i++) {
+   let rowContainer = document.createElement('div');
+   rowContainer.className = "grid-row";
+    for (let j = 0; j < 16; j++) {
         let boxDiv = document.createElement('div');
-        boxDiv.className= "grid-box";
-        container.appendChild(boxDiv);
+        boxDiv.className = "grid-column";
+        rowContainer.appendChild(boxDiv);
     }
+    container.appendChild(rowContainer);
 }
